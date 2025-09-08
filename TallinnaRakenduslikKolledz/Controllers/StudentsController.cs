fae71a1b75cf54b2d1b46305f6f96ce010701e28
pluuto <radzoffical@gmail.com>
2025-09-08 14:49:52 +0300

    Tegime õpilastele loomis akna ja paar meetodit kontrollerisse

--- TallinnaRakenduslikKolledz/Controllers/StudentsController.cs
+++ TallinnaRakenduslikKolledz/Controllers/StudentsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TallinnaRakenduslikKolledz.Data;
+using TallinnaRakenduslikKolledz.Models;
 
 namespace TallinnaRakenduslikKolledz.Controllers
 {
@@ -15,6 +16,25 @@ namespace TallinnaRakenduslikKolledz.Controllers
         public async Task<IActionResult> Index()
         {
             return View(await _context.Students.ToListAsync());
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID, LastName, FirstName, EnrollmentDate,HomeAddress, Age, Iq")] Student student)
+        {
+            if (ModelState.IsValid) 
+            {
+                _context.Students.Add(student);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+                // return RedirectToAction(nameof(Index))
+            }
+            return View(student);
+
         }
     }
 }
