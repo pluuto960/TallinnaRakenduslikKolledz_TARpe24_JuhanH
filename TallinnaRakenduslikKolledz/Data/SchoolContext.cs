59662836cc5ee3531288ccebd29c420a1d87f1fb
pluuto <radzoffical@gmail.com>
2025-09-15 14:29:42 +0300

    Added instructors model, assignments model. officeassignment model, instructors controller and model specifically for instructors index

--- TallinnaRakenduslikKolledz/Data/SchoolContext.cs
+++ TallinnaRakenduslikKolledz/Data/SchoolContext.cs
@@ -9,13 +9,21 @@ namespace TallinnaRakenduslikKolledz.Data
 
         public DbSet<Course> Courses { get; set; }
         public DbSet<Enrollment> Enrollments { get; set; }
-        public DbSet<Student> Students { get; set; }        
+        public DbSet<Student> Students { get; set; }   
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<CourseAssignment> CourseAssignments { get; set; }
+        public DbSet<OfficeAssignment> OfficeAssignment { get; set; }
+        
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Course>().ToTable("Course");
             modelBuilder.Entity<Student>().ToTable("Student");
             modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Instructor>().ToTable("Instructor");
+            modelBuilder.Entity<OfficeAssignment>().ToTable("OfficeAssignment");
+            modelBuilder.Entity<CourseAssignment>().ToTable("CourseAssignment");
 
         }
 
---
+++ TallinnaRakenduslikKolledz/Models/Instructor.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace TallinnaRakenduslikKolledz.Models
+{
+    public class Instructor
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Perekonnanimi")]
+        public string Lastname { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Eesnimi")]
+        public string FirstName { get; set; }
+        [Display(Name = "Õpetaja nimi")]
+        public string FullName
+        {
+            get { return Lastname + "," + FirstName; }
+        }
+
+        /**/
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}")]
+        [Display(Name = "Tööleasumiskuupäev")]
+        public DateTime HireDate {  get; set; }
+
+        public ICollection<CourseAssignment>? CourseAssignments {  get; set; }
+        public OfficeAssignment? OfficeAssignment { get; set; }
+
+        /* Lisa kolm omadust õpetajale*/
+
+        [Required]
+        [Display(Name = "Palk")]
+        public double Salary { get; set; }
+
+
+        [Display(Name = "Boonused")]
+        public string? Bonuses { get; set; }
+
+        [Required]
+        [StringLength(6)]
+        [Display(Name = "Sugu")]
+        public string Gender { get; set; }
+    }
+}
