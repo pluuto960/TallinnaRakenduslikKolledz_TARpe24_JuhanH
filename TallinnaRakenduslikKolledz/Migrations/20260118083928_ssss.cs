cc78175ff58b3155c5f8d0a7c9b57af067eda49b
pluuto960 <189347604+pluuto960@users.noreply.github.com>
2026-01-18 10:41:18 +0200

    Unustasin layouti lisada Delinquentsi

---
+++ TallinnaRakenduslikKolledz/Migrations/20260118083928_ssss.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TallinnaRakenduslikKolledz.Migrations
+{
+    /// <inheritdoc />
+    public partial class ssss : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Delinquents",
+                table: "Delinquents");
+
+            migrationBuilder.RenameTable(
+                name: "Delinquents",
+                newName: "Delinquent");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Delinquent",
+                table: "Delinquent",
+                column: "DelinquentId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Delinquent",
+                table: "Delinquent");
+
+            migrationBuilder.RenameTable(
+                name: "Delinquent",
+                newName: "Delinquents");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Delinquents",
+                table: "Delinquents",
+                column: "DelinquentId");
+        }
+    }
+}
